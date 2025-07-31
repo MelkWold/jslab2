61814a778b76e0fe90dba49e9fcd397f55ae9b8f
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-07-31 15:14:52 -0400

    first question

---
+++ README.md
@@ -0,0 +1,7 @@
+`Java Script Lab 2 Assignment`
+
+``This document contains the codes to answer the programming challenge for lab 2. ``
+
+`Part 1: Growing Pains`
+Instruction : Your task is to create a program that advises a group of environmental scientists how to handle the growth and spread of a unique plant species under their supervision. You must develop a growth control system that will monitor and predict the plant growth, making decisions based on the available space and potential growth.
+
---
+++ scripts.js
@@ -0,0 +1,26 @@
+// Part 1: Growing Pains
+// What do we know? Circular area with a radius of 5m; PI = 3.1415 area = Pi * radius * radius; plants double every week
+// const radius = 5;
+// const area = PI * radius * radius;
+// const spaceRequired = 0.8 * 0.8;
+// const initialPlantNumber = 20;
+// const rateOfGrowth = 2;
+// Task 1: Predict the plant growth after a specific number of weeks.
+
+let plantGrowth;
+let numberOfWeeks = 2; 
+const initialPlantNumber = 20;
+const rateOfGrowth = 2;
+
+{
+    plantGrowth = initialPlantNumber * rateOfGrowth * numberOfWeeks;
+    console.log (plantGrowth) 
+}
+
+
+
+
+// Part 2: 
+
+
+// Part 3: 
